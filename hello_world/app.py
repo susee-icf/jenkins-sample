0e6b6b47262de88117fcfc3e60ee68ad1198e527
SNatarajan <suseendran.natarajan@icf.com>
2022-04-14 17:12:36 -0400

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world jenkins Testing",
+            "message": "hello world jenkins Testing Working",
             # "location": ip.text.replace("\n", "")
         }),
     }
